9a805d225ea70ccdd9bfe70ff34a33690e5957e6
45P3R4 <51315442+45P3R4@users.noreply.github.com>
2024-04-16 02:08:39 +0300

    init

--- main.js
+++ main.js
@@ -1,41 +1,61 @@
-async function getData(url = "") {
-  const requestOptions = {
-    method: "GET",
-    redirect: "follow"
-  };
+window.onload = function () {
 
-  const response = await fetch(url, requestOptions);
-  return await response.json();
+  async function getData(url = "") {
+    const geo = ymaps.geolocation;
+    let userLocation = ymaps.geolocation.city;
+    const appid = "appid=85d298a42fe8487992bc681eaec51e81";
+    const lang = "lang=ru";
+    const units = "units=metric";
+    
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow"
+    };
+
+    document.getElementById("location-name")
+    .textContent = userLocation;
+
+    const response = await fetch(url + "?q=" + userLocation + "&" + appid + "&" + lang + "&" + units, requestOptions);
+    return await response.json();
+  }
+
+  let date = new Date().toLocaleString('ru', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  });
+
+  getData("https://api.openweathermap.org/data/2.5/weather")
+  .then((data) => {
+
+    document.getElementById("root").setAttribute("style", 
+    "background: url(./img/backgrounds/" + data.weather[0].icon + ".jpg) fixed; background-size: cover;")
+
+    document.getElementById("weather-description")
+    .textContent = data.weather[0].description;
+
+    document.getElementById("weather-icon")
+    .setAttribute("src", "./img/icons/" + data.weather[0].icon + ".png");
+
+    document.getElementById("weather-temp")
+    .textContent = "" + Math.round(data.main.temp) + "°C";
+
+    document.getElementById("weather-feels")
+    .textContent = "Ощущается как  " + Math.round(data.main.feels_like) + "°C";
+
+    document.getElementById("weather-pressure")
+    .textContent = "" + Math.round(data.main.pressure / 1.333) + " мм ртутного столба";
+
+    document.getElementById("weather-humidity")
+    .textContent = "Влажность " + data.main.humidity + "%";
+
+    document.getElementById("weather-wind-speed")
+    .textContent = "Ветер " + data.wind.speed + " м/c";
+
+    document.getElementById("weather-location")
+    .textContent = data.name;
+
+    document.getElementById("weather-date")
+    .textContent = date;
+  })
 }
-
-getData("https://api.openweathermap.org/data/2.5/weather?q=Voronezh&appid=85d298a42fe8487992bc681eaec51e81&lang=ru&units=metric")
-.then((data) => {
-
-  document.getElementById("root").setAttribute("style", 
-  "background: url(./img/backgrounds/" + data.weather[0].icon + ".jpg) fixed; background-size: cover;")
-
-  document.getElementById("weather-description")
-  .textContent = data.weather[0].description + " " + Math.round(data.main.temp) + "°C";
-
-  document.getElementById("weather-icon")
-  .setAttribute("src", "./img/icons/" + data.weather[0].icon + ".png");
-
-  document.getElementById("weather-temp")
-  .textContent = "Температура " + Math.round(data.main.temp) + "°C";
-
-  document.getElementById("weather-feels")
-  .textContent = "Ощущается как  " + Math.round(data.main.feels_like) + "°C";
-
-  document.getElementById("weather-pressure")
-  .textContent = "Давление " + Math.round(data.main.pressure / 1.333) + " мм ртутного столба";
-
-  document.getElementById("weather-humidity")
-  .textContent = "Влажность " + data.main.humidity + "%";
-
-  document.getElementById("weather-wind-speed")
-  .textContent = "Ветер " + data.wind.speed + " м/c";
-
-  document.getElementById("weather-wind-deg")
-  .textContent = "Направление ветра " + data.wind.deg + " градусов";
-
-})
